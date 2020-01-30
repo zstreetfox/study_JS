51dc34d982e9547d10bf0a9e4bbda1e506e0764d
Mankevich Stanislav <zstreetfox@gmail.com>
2020-01-31 00:26:11 +0200

    модифицировал переменные, добавил циклы

--- lesson1/scripts.js
+++ lesson1/scripts.js
@@ -1,7 +1,13 @@
 
 'use strict';
 
-let money =  +prompt('Ваш месячный доход?', 7777),
+let isNumber = function(n) {
+return !isNaN(parseFloat(n)) && isFinite(n)
+};
+
+
+
+let money,
     money2,
     income = 'freelance', 
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период, через запятую'),
@@ -9,13 +15,23 @@ let money =  +prompt('Ваш месячный доход?', 7777),
     deposit = confirm('Есть ли у вас депозит в банке?'), 
     deposit2, 
     mission = 99999, 
-    period = 4,
+    period = 3,
     budgetMonth,
     numArr,
     arrLength,
     allCosts,
     i,
+    start,
     budgetDay;
+
+    start = function() {
+  do {
+    money = +prompt('Ваш месячный доход?');
+  } 
+  while (!isNumber(money));
+};
+start();
+console.log(money);
 
     let showTyoeOf = function (data) {
         console.log(data, typeof(data));
@@ -28,31 +44,11 @@ let money =  +prompt('Ваш месячный доход?', 7777),
     // console.log('Период равен ' + period + ' месяцев'  + '\nЦель заработать ' + mission + ' ,dollars');
     // console.log(addExpenses.toLowerCase());
     // console.log(addExpenses.split(' '));
-    // budgetDay = money/30;
+    budgetDay = money/30;
     // console.log('Бюджет на день равен :' , budgetDay);
+
     
-    
-    
-    
-    
-    
-//     budgetMonth = money - allCosts;
-
-
-// console.log('Бюджет на месяц составит : ' , budgetMonth);
-
-// i = mission / budgetMonth;
-
-// console.log('Миссия будет завершена через ' , Math.ceil(i), 'месяцев');
-
-// budgetDay = budgetMonth/30;
-
-// console.log('Скорректированный бюджет на день составит :' ,Math.floor(budgetDay) );
-
-
-
-
-numArr = addExpenses.split(',');
+    numArr = addExpenses.split(',');
     arrLength = addExpenses.length;
     money = Number(money);
     allCosts = 0;
@@ -60,11 +56,41 @@ numArr = addExpenses.split(',');
         numArr[i] = numArr[i] * 1;
         allCosts = allCosts + numArr[i];
         
-    };
+    }
+
+    budgetMonth = money - allCosts;
+
+console.log('Бюджет на месяц составит : ' , budgetMonth);
+
+i = mission / budgetMonth;
+
+
+console.log('Скорректированный бюджет на день составит :' ,Math.floor(budgetDay) );
+
+console.log(money);
+let getStatusIncome = function () {
+if (budgetDay >= 300){
+    return('У вас высокий уровень дохода');
+} else if (budgetDay < 300) {
+    return('У вас средний уровень дохода');
+} else if (budgetDay <= 0) {
+    return('Что то пошло не так');
+}
+else if(budgetDay <= 150) {
+    return('К сожалению у вас уровень дохода ниже среднего');
+}
+};
+console.log (getStatusIncome());
 
 let getExpensesMonth = function (){
+    do {
+        allCosts = +prompt('Во сколько это обойдется?');
+      } 
+      while (!isNumber(allCosts));
+
     return allCosts;
 };
+getExpensesMonth();
 console.log('Расходы за месяц составят : ' , allCosts);
 let getAccumulatedMonth = function (){
 
@@ -78,25 +104,22 @@ let accumulatedMonth = getAccumulatedMonth();
 
 let getTargetMonth = function (){
     return mission / accumulatedMonth;
+   
 };
-console.log('Cрок достижения цели в месяцах:',  Math.ceil(getTargetMonth()));
+
+
+let getStatusIncome2 = function () {
+if  (getTargetMonth() < 0){
+    console.log('Цель не будет достигнута');
+} else if( getTargetMonth() > 0 ){
+    return(console.log('Cрок достижения цели в месяцах:'
+    , Math.ceil(getTargetMonth())));
+}
+};
+getStatusIncome2();
+console.log(getTargetMonth());
 
 budgetDay = accumulatedMonth / 30;
 
 console.log('бюджет на день составит :' ,Math.floor(budgetDay) );
 
-let getStatusIncome = function () {
-    if (budgetDay >= 300){
-        return('У вас высокий уровень дохода');
-    } else if (budgetDay <= 300) {
-        return('У вас средний уровень дохода');
-    } else if (budgetDay <= 0) {
-        return('Что то пошло не так');
-    }
-    else if(budgetDay <= 150) {
-        return('К сожалению у вас уровень дохода ниже среднего');
-    }
-    };
-    console.log (getStatusIncome());
-
-console.log (addExpenses);
